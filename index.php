fc5e37cb476bb1df0876d35c5d3fd2090607741d
AngryKiwi <129200338+VeryAngrykiwi@users.noreply.github.com>
2024-03-11 13:08:47 +0000

    complete trials

--- index.php
+++ index.php
@@ -3,8 +3,21 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hi, Bro, i\'m started!</p>'; ?> 
-
+    <?php 
+      function isPowerOfThree($num){
+        if($num === 1){
+          return true;
+        }
+        while($num > 1){
+          if($num % 3 !== 0){
+            return 'не троичное';
+          }
+          $num /= 3;
+        }
+        return 'троичное';
+      }
+      print_r(isPowerOfThree(9));
+    ?>
   <!--
   This script places a badge on your repl's full-browser view back to your repl's cover
   page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
